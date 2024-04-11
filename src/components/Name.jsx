16294c6d54c89e2ac4bc44decff444f01b94f4ec
Yoo <tmddls91@inha.edu>
2024-04-11 11:37:13 +0900

    React components

--- src/components/Name.jsx
+++ src/components/Name.jsx
@@ -1,14 +1,27 @@
 import React from 'react'
+import { useState } from 'react'
 
 const Name = () => {
+  const [name,setName] = useState('');
+  const [nickname,setNickname] = useState('') 
+
+  const ChangeName = e => {
+    setName(e.target.value);
+  }
+
+  const ChanegeNickname = e => {
+    setNickname(e.target.value);
+  }
+
+  
   return (
     <div className="name">
       <h>Name</h>
       <hr></hr>
-      <input name="myName" placeholder="이름을 입력해주세요" />
-      <input name="nickName" placeholder="닉네임을 입력해주세요" />
-      <p>이름</p>
-      <p>닉네임</p>
+      <input value = {name} onChange = {ChangeName} name="myName" placeholder="이름을 입력해주세요" />
+      <input value = {nickname} onChange = {ChanegeNickname} name="nickName" placeholder="닉네임을 입력해주세요" />
+      <p>이름: {name}</p>
+      <p>닉네임: {nickname} </p>
     </div>
   )
 }
